96b95afbba98ba74139b293d2b4998aade83eff1
dayclear <day_clear@163.com>
2017-04-08 18:47:36 +0800

    组合

--- practice.js
+++ practice.js
@@ -324,3 +324,30 @@ function permutate(array, permutatedArray) {
 	return permutatedArray;
 }
 
+//组合
+function choose(arr, size) {
+	var allResult = [];
+	(function(arr, size, result) {
+		var arrLen = arr.length;
+		if (size > arrLen) {
+			return;
+		}
+		if (size == arrLen) {
+			allResult.push([].concat(result, arr))
+		} else {
+			for (var i = 0; i < arrLen; i++) {
+				var newResult = [].concat(result);
+				newResult.push(arr[i]);
+
+				if (size == 1) {
+					allResult.push(newResult);
+				} else {
+					var newArr = [].concat(arr);
+					newArr.splice(0, i + 1);
+					arguments.callee(newArr, size - 1, newResult);
+				}
+			}
+		}
+	})(arr, size, []);
+	return allResult;
+}
